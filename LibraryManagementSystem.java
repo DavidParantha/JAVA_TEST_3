dc0f88e339ace48ad8ef836c36f2a8432102d557
David Parantha <150181814+DavidParantha@users.noreply.github.com>
2026-02-16 10:55:49 +0530

    Update LibraryManagementSystem.java

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,7 +1,18 @@
 package dev.underthis.Abstraction.test3;
-
-public class LibraryManagementSystem {
-    public static void main (String[] args){
-
+     class Book {
+    String isbn;
+    String title;
+    boolean isAvailable;
+    public Book(String isbn, String title, boolean isAvailable) {
+        this.isbn = isbn;
+        this.title = title;
+        this.isAvailable = isAvailable;
+    }
+    class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message) {
+        super(message);
     }
 }
+    }
+}
+
